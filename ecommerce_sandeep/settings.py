b6e239f29f1065d83facad8f455e1d8c8160ad52
Sanddy00714 <sanddy00714@gmail.com>
2022-07-03 10:19:28 +0545

    template

--- ecommerce_sandeep/settings.py
+++ ecommerce_sandeep/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            
+
         },
     },
 ]
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR, "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
